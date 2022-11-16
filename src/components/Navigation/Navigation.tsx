a86c082f7d700e20c491ce3c3479125678105437
BlackH3art <jaroslaw.musielak@outlook.com>
2022-11-16 20:22:56 +0100

    add: logo

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react"
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import logo from '../../images/logo-rect.svg';
 
 export const Navigation: FC = () => {
 
@@ -8,7 +9,7 @@ export const Navigation: FC = () => {
       <nav className="w-full fixed z-10 h-20 bg-[#11111125] border-b-[1px] border-gray-800 backdrop-blur-md">
         <div className="flex items-center justify-between w-4/5 mx-auto h-full">
 
-          <p>dLotto</p>
+          <img src={logo} className="h-20" alt="" />
           <ConnectButton />
 
         </div>
